393cb5698b8d397b063153af4f7aadf156e305bc
Lambda <lambda@Lambdas-MacBook-Pro-3.local>
2019-10-09 14:21:59 +0100

    merge_sort attempt

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,28 +1,58 @@
 # TO-DO: Complete the selection_sort() function below 
 def selection_sort( arr ):
+    
     # loop through n-1 elements
     for i in range(0, len(arr)):
-        cur_index = i
-        smallest_index = cur_index
+        # cur_index = i
+        # smallest_index = cur_index
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-        
-        if(arr[i] < arr[smallest_index]):
-            smallest = i
+        # (hint, can do in 3 loc)
+        for x in range(i+1, len(arr)):
+            if arr[x] < arr[i]:
+                mix = arr[x]
+                arr[x] = arr[i]
+                arr[i] = mix 
         # TO-DO: swap
-            a = i
-            b = smallest
-            arr[a], arr[b] = arr[b], arr[a]
-    return print(arr)
 
-selection_sort([7,5,1,4,2])
-# # TO-DO:  implement the Bubble Sort function below
+    return arr
+#  it two time loop.
+#  the first is the base while the other is forward
+#  so at all time the first loop is loop for min then jumps to the next index
+#  to get an understanding print print(i, x)
+# print(selection_sort([7, 5, 4, 1, 2]))
+# TO-DO:  implement the Bubble Sort function below
+
 # def bubble_sort( arr ):
-
+#     for i in range(0, len(arr)):
+#         for x in range(i+1, len(arr)):
+#             current = i
+#             if arr[current] > arr[x]:
+#                 a = arr[x]
+#                 arr[x] = arr[current]
+#                 arr[current] = a
+#                 current = x
 #     return arr
+def bubble_sort( arr ):
+    lastSorted = len(arr) - 1 
+    isSorted = False
+    while not isSorted:
+        isSorted = True
+        for i in range(0, lastSorted):
+            if arr[i] > arr[i+1]:
+                a = arr[i]
+                b = arr[i+1]
+                arr[i] = b
+                arr[i+1] = a
+                isSorted = False
+        --lastSorted
+    return arr
+print(bubble_sort([7, 5, 4, 1, 2]))
+# for bubble you are working with the highest and trying to push it to the right end
+# you also have to keep the state if the current you swap it
+# then it change when the upper iteration changes  
 
-
-# # STRETCH: implement the Count Sort function below
+# STRETCH: implement the Count Sort function below
 # def count_sort( arr, maximum=-1 ):
 
 #     return arr
+
--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,8 +1,19 @@
+import math
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
 def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
-    merged_arr = [0] * elements
+    # elements = len( arrA ) + len( arrB )
+    # merged_arr = [0] * elements
     # TO-DO
+    merged_arr = []
+    while(len(arrA) and len(arrB)):
+        if(arrA[0] < arrB[0]):
+            merged_arr.append(arrA.pop(0))
+        else:
+            merged_arr.append(arrB.pop(0))
+    while(len(arrA)):
+        merged_arr.append(arrA.pop(0))
+    while(len(arrB)):
+        merged_arr.append(arrB.pop(0))
     
     return merged_arr
 
@@ -10,10 +21,17 @@ def merge( arrA, arrB ):
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
+    if(len(arr) < 2):
+        return arr
+    middle = math.floor(len(arr)/2)
+    left = arr[:middle]
+    right = arr[middle:]
+    arrA = merge_sort(left)
+    arrB = merge_sort(right)
+    return merge(arrA,arrB)
 
-    return arr
 
-
+print(merge_sort([1, 5, 8, 4, 2, 9, 6, 0, 3, 7]))
 # STRETCH: implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
     # TO-DO
